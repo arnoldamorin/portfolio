0ccbc5194c3940ea69fbecff7f689ac138c89459
arnoldamorin <aamorin.ar@gmail.com>
2021-02-01 15:48:40 -0300

    updates

--- Proyectos/SGV/entidades/config.php
+++ Proyectos/SGV/entidades/config.php
@@ -4,21 +4,14 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
 //Iniciamos la session
-session_start();
+//session_start();
 
 class Config {
-    const BBDD_HOST = "192.168.0.210";
-    const BBDD_USUARIO = "arnoldamorin";
-    const BBDD_CLAVE = " 4EVJ.8678";
-    const BBDD_NOMBRE = "arnoldamorin";
+    const BBDD_HOST = "54.39.143.244:3306";
+    const BBDD_USUARIO = "abertur6_admin";
+    const BBDD_CLAVE = " puedeserpa666";
+    const BBDD_NOMBRE = "abertur6_arnoldamorin";
 }
 
-$mysqli = new mysqli(config::BBDD_HOST, config::BBDD_USUARIO, config::BBDD_CLAVE, config::BBDD_NOMBRE);  
-$sql = "SELECT count(V.fk_idcliente) AS totalcliente                      
-        FROM clientes C inner join Ventas V on V.fk_idcliente = C.idcliente
-        WHERE C.idcliente = $id";
-        
-if (!$mysqli->query($sql)){
-    printf("Error en query: %s\n", $mysqli->error . " " .$sql);             
-} 
+ 
 ?>
